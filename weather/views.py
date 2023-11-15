cf7e443af120a83bb29ac89d32c94060636489e3
TymoteuszUjek <135643494+TymoteuszUjek@users.noreply.github.com>
2023-11-15 13:52:42 +0100

    Update views.py

--- weather/views.py
+++ weather/views.py
@@ -8,7 +8,7 @@ def index(request):
     if request.method == 'POST':
         city = request.POST.get('city')
         if city:
-            geo_url = f'http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid='
+            geo_url = f'http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid=191c5900fd29178d8c8756a1d5a76c55'
             geo_response = requests.get(geo_url)
             if geo_response.status_code == 200:
                 geo_data = geo_response.json()
@@ -16,7 +16,7 @@ def index(request):
                     lat = geo_data[0]['lat']
                     lon = geo_data[0]['lon']
                     
-                    weather_url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid='
+                    weather_url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid=191c5900fd29178d8c8756a1d5a76c55'
                     weather_response = requests.get(weather_url)
                     if weather_response.status_code == 200:
                         weather_data = weather_response.json()
